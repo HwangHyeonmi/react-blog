caf39aa0f59cddeb1c8ff0cb4f00846af82a56c5
HwangHyeonmi <49122297+HwangHyeonmi@users.noreply.github.com>
2020-11-19 16:20:23 +0900

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://brownrice0916:abcd1234@cluster0.qldcm.mongodb.net/<dbname>?retryWrites=true&w=majority', 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const config = require('./config/key')
+
+const { User } = require('./models/user');
+
+
+mongoose.connect(config.mongoURI, 
 {useUnifiedTopology: true,
     useNewUrlParser: true}).then(() => console.log('DB connected'))
                         .catch(err =>console.error(err));
 
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get('/',(req,res) =>{
+    res.json({"hello ~":"Hi~~ dfsdssfdsds"})
+})
+
+
+app.post('/api/users/register',(req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, userData) => {
+        if(err) return res.json ({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+    
+})
+
+
+
+
 
 
 app.get('/', (req,res)=>{
-    res.send('hello wold')
+    res.send('hello world')
 });
 
 
